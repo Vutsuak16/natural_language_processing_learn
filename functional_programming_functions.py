f030a6a54c0529e7a67b951ad6c42a4771e1e1e8
kaustuv deolal <vutsuak96@gmail.com>
2016-04-27 01:55:30 +0530

    upper lower used with map

--- functional_programming_functions.py
+++ functional_programming_functions.py
@@ -1,6 +1,8 @@
 k = [1, 2, 3, 4, 5]
 l = [1, 4, 9, 16, 25]
 m = [1, 8, 64, 125, 216]
+lower = ['a', 'b', 'c']
+upper = ['A', 'B', 'C']
 
 lamb = lambda x, y: x + y
 for i in range(5):
@@ -18,3 +20,21 @@ print("\n")
 filtered = filter(lambda x: x % 2 == 0, m)
 for i in filtered:
     print(i, end=" ")
+
+print("\n")
+
+# no reduce in python 3 use functools.reduce
+
+to_lower = map(lambda x: x.lower(), upper)
+
+for i in to_lower:
+    print(i, end=" ")
+
+print("\n")
+
+to_upper = map(lambda x: x.upper(), lower)
+
+for i in to_upper:
+    print(i, end=" ")
+
+print("\n")
