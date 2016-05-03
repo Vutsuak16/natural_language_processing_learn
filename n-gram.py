2e2ab40606278910ccf2a5c9e93cc8f66ce2f22b
kaustuv deolal <vutsuak96@gmail.com>
2016-05-03 17:19:03 +0530

    n-gram.py added

---
+++ n-gram.py
@@ -0,0 +1,15 @@
+from itertools import islice
+
+l = ["kaustuv", "is", "a", "good", "boy", "code", "is", "life", "hi", "guys", "jai", "hanuman"]
+k = []
+n = 3
+try:
+    for i in range(0, len(l)):
+        x = []
+        for j in range(i, n + i):
+            x.append(l[j])
+        k.append(x)
+except:
+    pass
+
+print(k)
