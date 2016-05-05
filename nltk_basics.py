899b79bad63df8b8eceb7da40e0d6c4b2ce24ac0
kaustuv deolal <vutsuak96@gmail.com>
2016-05-06 02:40:06 +0530

    nltk basics added

---
+++ nltk_basics.py
@@ -0,0 +1,5 @@
+# tokenizing
+from nltk.tokenize import sent_tokenize,word_tokenize
+sentence = "Ram is a good boy. He likes arrow.He is nice like that.He is awesome "
+print(sent_tokenize(sentence))
+print(word_tokenize(sentence))
