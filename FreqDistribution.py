e3a5ad275a69a97106fb643249d6f80250ee5903
kaustuv deolal <vutsuak96@gmail.com>
2016-05-16 10:08:57 +0530

    Frequency Dist puzzle

---
+++ FreqDistribution.py
@@ -0,0 +1,8 @@
+from nltk import FreqDist
+import nltk
+# solves problem given in text 61 of nltk book
+
+puzzle_letter = FreqDist("egivrvonl")
+obligatory='r'
+#wordlist=nltk.corpus.words.words()
+print(FreqDist)
