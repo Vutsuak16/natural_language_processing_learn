0b57c5fdc987502a07894ad28b6b8bd5cb75bdcf
kaustuv deolal <vutsuak96@gmail.com>
2016-05-16 14:14:20 +0530

    FreqDistribution.py modified puzzle cracked

--- FreqDistribution.py
+++ FreqDistribution.py
@@ -2,7 +2,9 @@ from nltk import FreqDist
 import nltk
 # solves problem given in text 61 of nltk book
 
-puzzle_letter = FreqDist("egivrvonl")
+puzzle_letters = FreqDist("egivrvonl")
 obligatory='r'
-#wordlist=nltk.corpus.words.words()
-print(FreqDist)
+wordlist=nltk.corpus.words.words()
+l=[w for w in wordlist if len(w) >= 6 and obligatory in w and nltk.FreqDist(w) <= puzzle_letters]
+for i in l:
+    print(i,end=" ")
